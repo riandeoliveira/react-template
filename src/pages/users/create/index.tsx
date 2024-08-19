4a2d8e953836b50fdfa675980ff1565893c67ee7
Rian Oliveira <riandiasdeoliveira2001@gmail.com>
2024-08-18 23:02:19 -0300

    feat: add missing private routes

--- src/pages/users/create/index.tsx
+++ src/pages/users/create/index.tsx
@@ -1,13 +1,16 @@
 import { NavigationPanelLayout } from "@/layouts/navigation-panel-layout";
+import { PrivateRoute } from "@/routes/private-route";
 import type { ReactElement } from "react";
 
 const UsersCreatePage = (): ReactElement => {
   return (
-    <NavigationPanelLayout title="Criação de Usuário">
-      <div>
-        <p>Esta é a página de criação de usuário.</p>
-      </div>
-    </NavigationPanelLayout>
+    <PrivateRoute>
+      <NavigationPanelLayout title="Criação de Usuário">
+        <div>
+          <p>Esta é a página de criação de usuário.</p>
+        </div>
+      </NavigationPanelLayout>
+    </PrivateRoute>
   );
 };
 
--- src/pages/users/listing/index.tsx
+++ src/pages/users/listing/index.tsx
@@ -1,13 +1,16 @@
 import { NavigationPanelLayout } from "@/layouts/navigation-panel-layout";
+import { PrivateRoute } from "@/routes/private-route";
 import type { ReactElement } from "react";
 
 const UsersListingPage = (): ReactElement => {
   return (
-    <NavigationPanelLayout title="Listagem de Usuários">
-      <div>
-        <p>Esta é a página de listagem de usuários.</p>
-      </div>
-    </NavigationPanelLayout>
+    <PrivateRoute>
+      <NavigationPanelLayout title="Listagem de Usuários">
+        <div>
+          <p>Esta é a página de listagem de usuários.</p>
+        </div>
+      </NavigationPanelLayout>
+    </PrivateRoute>
   );
 };
 
